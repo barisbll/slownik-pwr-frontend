6fcc82cca57f71e01ac8c46ad2263c984c4fe295
Baris Balli <barisbll.dev@gmail.com>
2022-07-16 14:35:43 +0200

    env variable logic added to the next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        host: "localhost",
+        port: "3000",
+      },
+    };
+  }
+
+  return {
+    reactStrictMode: true,
+    env: {
+      host: "https://slownik-pwr-backend.herokuapp.com/",
+      port: "80",
+    },
+  };
+};
